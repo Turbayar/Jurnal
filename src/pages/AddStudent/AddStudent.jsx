bd7a38d74421ab0ec79c3272cbe8a9b4152f3548
Turbayar <Tito@Bilguuns-iMac.local>
2022-05-31 17:22:42 +0800

    completed

--- src/pages/AddStudent/AddStudent.jsx
+++ src/pages/AddStudent/AddStudent.jsx
@@ -26,7 +26,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "80%",
+  width: "50%",
   bgcolor: "background.paper",
   border: "2px solid #ced4da",
   boxShadow: 24,
