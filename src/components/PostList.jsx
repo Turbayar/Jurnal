3638802055473511f8101906260948bed1dcffd1
Turbayar <Tito@Bilguuns-iMac.local>
2022-05-31 12:58:53 +0800

    oreder by asc

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
 import Post from "./Post";
-import { collection, getDocs, onSnapshot, doc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  doc,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 export default function PostList() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
-    const unsub = onSnapshot(collection(db, "students"), (col) => {
-      const myList = col.docs.map((doc) => {
-        return {
-          id: doc.id,
-          ...doc.data(),
-        };
-      });
-      setPosts(myList);
-    });
+    const unsub = onSnapshot(
+      query(collection(db, "students"), orderBy("firstName", "asc")),
+      (col) => {
+        const myList = col.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setPosts(myList);
+      }
+    );
 
     return () => unsub();
   }, []);
